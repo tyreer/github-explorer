2c678ff558c5676301c3179fd630dceb962b41b1
Robert Tyree <tyreer@gmail.com>
2017-05-05 16:01:24 +0100

    Made SelectLanguage function into its own component

--- app/components/Famous.js
+++ app/components/Famous.js
@@ -1,12 +1,43 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+class SelectLanguage extends React.Component {
+  render() {
+    const languages = ['All', 'JavaScript', 'CSS'];
+    return(
+      <div>
+        <h1>FAMOUS: {this.props.selectedLanguage}</h1>
+        <ul className = 'languages'>
+          {languages.map(language => {
+            return (
+              <li
+                style={language === this.props.selectedLanguage ? {color: 'white'}: null}
+                key={language}
+                onClick={() => {this.props.updateLanguage(language)}}
+                >
+                {language}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  updateLanguage: PropTypes.func.isRequired,
+}
+
 class Famous extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedLanguage: 'All'
     };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
   }
 
   updateLanguage(language) {
@@ -14,24 +45,11 @@ class Famous extends React.Component {
   }
 
   render() {
-    const languages = ['All', 'JavaScript', 'CSS'];
-
     return(
       <div>
-        <h1>FAMOUS: {this.state.selectedLanguage}</h1>
-        <ul className = 'languages'>
-          {languages.map(language => {
-            return (
-              <li
-                style={language === this.state.selectedLanguage ? {color: 'white'}: null}
-                key={language}
-                onClick={() => {this.updateLanguage(language)}}
-                >
-                {language}
-              </li>
-            )
-          })}
-        </ul>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          updateLanguage={this.updateLanguage} />
       </div>
     )
   }
