3f507df3f06640a7fdcadea3b4c51b33f94a2dd1
Robert Tyree <tyreer@gmail.com>
2017-05-05 12:33:57 +0100

    Added state and onClick functionality to Famous component

--- app/components/App.js
+++ app/components/App.js
@@ -2,29 +2,29 @@ var React = require('react');
 var Famous = require('./Famous');
 var Megaphone = require('./Megaphone');
 
-const SHOUT_DATA = [
-    {
-      id: 1,
-      content: 'YOO'
-    },
-    {
-      id: 2,
-      content: 'NOO'
-    },
-    {
-      id: 3,
-      content: 'OOOOOOO'
-    },
-    {
-      id: 4,
-      content: 'BOOOO'
-    }
-];
-
 class App extends React.Component {
   render() {
+    const SHOUT_DATA = [
+        {
+          id: 1,
+          content: 'YOO'
+        },
+        {
+          id: 2,
+          content: 'NOO'
+        },
+        {
+          id: 3,
+          content: 'OOOOOOO'
+        },
+        {
+          id: 4,
+          content: 'BOOOO'
+        }
+    ];
+
     return (
-      <div>
+      <div className='container'>
         <Famous />
         <Megaphone data={SHOUT_DATA} color={'white'} />
       </div>
--- app/components/Famous.js
+++ app/components/Famous.js
@@ -2,9 +2,37 @@ var React = require('react');
 var PropTypes = require('prop-types');
 
 class Famous extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+  }
+
+  updateLanguage(language) {
+    this.setState( { selectedLanguage: language } );
+  }
+
   render() {
+    const languages = ['All', 'JavaScript', 'CSS'];
+
     return(
-      <div><h1>FAMOUS</h1></div>
+      <div>
+        <h1>FAMOUS: {this.state.selectedLanguage}</h1>
+        <ul className = 'languages'>
+          {languages.map(language => {
+            return (
+              <li
+                style={language === this.state.selectedLanguage ? {color: 'white'}: null}
+                key={language}
+                onClick={() => {this.updateLanguage(language)}}
+                >
+                {language}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     )
   }
 }
