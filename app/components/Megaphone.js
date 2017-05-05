0be0d85794f671ca78d8789fbb355a1b4e6b2144
Robert Tyree <tyreer@gmail.com>
2017-05-05 11:16:18 +0100

    Adjusted styling and fixed inline style for Megaphone component to accept prop as passed in

--- app/components/Megaphone.js
+++ app/components/Megaphone.js
@@ -4,8 +4,8 @@ var PropTypes = require('prop-types');
 class Megaphone extends React.Component {
   render() {
     return (
-      <div style={{color: this.props.data.color}}>
-        <h1>All shouts</h1>
+      <div style={{color: this.props.color}}>
+        <h1 >All shouts</h1>
         <ul>
           {this.props.data.map(shout => <li key={shout.id}>{shout.content}</li>)}
         </ul>
