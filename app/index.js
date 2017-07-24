c3affe6f25ce51cc239cd29e0be335f2ecfef37d
Robert Tyree <tyreer@gmail.com>
2017-07-24 23:16:35 +0100

    Added PostCSS and CSSNext via Webpack

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
-require('./index.css');
+import styles from './index.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './app/index.js',
@@ -12,18 +13,30 @@ const config = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 1 },
+            },
+            'postcss-loader',
+          ],
+        }),
+      },
       { test: /\.(png|jpg|svg)$/, use: 'url-loader' },
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-  },
   plugins: [
+    new ExtractTextPlugin('index.css'),
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
-  ]
+  ],
+  devServer: {
+    historyApiFallback: true,
+  }
 };
 
 if (process.env.NODE_ENV === 'production') {
