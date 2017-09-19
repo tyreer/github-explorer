ecd26b3187a689e8e7be922fa7a2391b03d4a067
Robert Tyree <tyreer@gmail.com>
2017-09-19 08:55:39 +0100

    Revised nav styling

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,27 +3,35 @@ import { NavLink } from 'react-router-dom';
 
 export default function Nav () {
   return (
-    <ul className='nav'>
-      <li>
-        <NavLink exact activeClassName='active' to='/'>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink activeClassName='active' to='/followers'>
-          Followers
-        </NavLink>
-      </li>
-      <li>
-        <NavLink activeClassName='active' to='/battle'>
-          Battle
-        </NavLink>
-      </li>
-      <li>
-        <NavLink activeClassName='active' to='/famous'>
-          Famous
-        </NavLink>
-      </li>
-    </ul>
+    <div>
+      <ul className='nav'>
+        <li>
+          <NavLink exact activeClassName='active' to='/'>
+            Home
+          </NavLink>
+        </li>
+        <li>
+        </li>
+        <li>
+        </li>
+      </ul>
+      <ul className='nav--sub'>
+        <li>
+          <NavLink activeClassName='active' to='/followers'>
+            Followers
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName='active' to='/battle'>
+            Battle
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName='active' to='/famous'>
+            Famous
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   )
 }
